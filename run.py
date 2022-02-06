e9663c6bfefa7dcb79556b13226e6564ec1ac7f3
Gareth-McGirr <gareth.mcgirr@hotmail.com>
2022-02-06 22:20:45 +0000

    Setup MongoDB connection and save vehicle functionality

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
-from os import system
+import os
+from pymongo import MongoClient
+
+if os.path.exists("env.py"):
+    import env
+
+cluster = os.environ.get("CLUSTER")
+
+client = MongoClient(cluster)
 
 def main_menu():
     """
     Displays the main menu ion the terminal
     """
-    system('clear')
+    os.system('clear')
     print("MAIN MENU")
     print("---------------")  
     print("1. Vehicles")
@@ -31,7 +39,7 @@ def vehicle_menu():
 
     """
    
-    system('clear')
+    os.system('clear')
     print("VEHICLE MENU")
     print("---------------")
     print("1. Add new")
@@ -44,7 +52,7 @@ def vehicle_menu():
 
     if choice == "1":
         new_vehicle = get_new_vehicle_details()
-        print(new_vehicle)
+        save_vehicle_details(new_vehicle)
     elif choice == "2":
         print("Not Implemented")
     elif choice == "3":
@@ -63,7 +71,7 @@ def booking_menu():
 
     """
    
-    system('clear')
+    os.system('clear')
     print("BOOKING MENU")
     print("---------------")
     print("1. New Booking")
@@ -91,7 +99,7 @@ def report_menu():
 
     """
    
-    system('clear')
+    os.system('clear')
     print("REPORT MENU")
     print("---------------")
     print("1. Todays Bookings")
@@ -131,12 +139,19 @@ def get_new_vehicle_details():
     return vehicle
 
 
+def save_vehicle_details(vehicle_dict_string):
+    """
+    save the vehicle details to database
+    """
+    db = client.rentalsDB
+    vehicles = db.vehicles
+    result = vehicles.insert_one(vehicle_dict_string)
+
 def main():
     """
     Run all program functions
     """
     main_menu()
-
 
 print("\n\nWelcome to Motorcycle Rental Management.\n")
 main()
